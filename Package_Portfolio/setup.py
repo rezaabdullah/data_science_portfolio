966a5167f05fbd86bb0631ec8d06d7cad2e667fc
rezaabdullah <air.reza@hotmail.com>
2020-07-28 14:50:07 +0600

    feat: README & setup updated

--- Package_Portfolio/setup.py
+++ Package_Portfolio/setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
-setup(name = "distributions",
-        version = "0.1",
+setup(name = "point-distance",
+        version = "1.0",
         description = "Find distance between points",
-        packages = ["distributions"],
+        packages = ["point-distance"],
+        author = "Abdullah Reza"
         zip_safe = False)
