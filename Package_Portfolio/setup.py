601fe7371df46cac231aef9862f5335987ebdda4
rezaabdullah <air.reza@hotmail.com>
2020-07-31 21:00:18 +0600

    fix: Remove cache files

--- Package_Portfolio/setup.py
+++ Package_Portfolio/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(name = "point-distance",
-        version = "1.1",
+setup(name = "pointdistance",
+        version = "1.0",
         description = "Find distance between points",
-        packages = ["point-distance"],
+        packages = ["pointdistance"],
         author = "Abdullah Reza",
         zip_safe = False)
