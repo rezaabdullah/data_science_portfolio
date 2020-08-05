d77e1a48121261fd44de1e081c7ae6a8cabc4b88
rezaabdullah <air.reza@hotmail.com>
2020-08-05 19:44:07 +0600

    docs: Added docstring for methods

--- disaster_response_pipeline/process_data.py
+++ disaster_response_pipeline/process_data.py
@@ -6,21 +6,23 @@ from sqlalchemy import create_engine
 # Load datasets
 def load_data(messages_filepath, categories_filepath):
     """
-    Read .csv files for messages and categories dataset and merge it into one dataframe
+    Method for reading two CSV files and merge into one pandas dataframe
     
-    input:
-        messages_filepath: The .csv file path of messages dataset
-        categories_filepath: The .csv file path of categories dataset
+    The function reads CSV file of messages and categories and merge them into one pandas dataframe
+
+    Args:
+        messages_filepath (str): The .csv file path of messages dataset
+        categories_filepath (str): The .csv file path of categories dataset
         
     output:
-        df: Return merged dataset
+        df (pandas dataframe): Return merged dataset of messages and categories
     """
     
     # Read input dataset
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     
-    # Merge input dataframes
+    # Merge two dataframes into one dataframe
     df = pd.merge(messages, categories, on = "id", how = "outer")
     
     return df
@@ -28,13 +30,13 @@ def load_data(messages_filepath, categories_filepath):
 # Clean the dataset
 def clean_data(df):
     """
-    Clean the merged dataset from load_data
+    Method for cleaning the dataframe obtained from 'load_data'
     
-    input:
-        df: The merged dataset from load_data
+    Args:
+        df (pandas dataframe): The merged dataframe from load_data
     
     output:
-        df: Clean dataset
+        df (pandas dataframe): Sanitized dataframe
     """
     
     # Split categories into separate category columns
@@ -55,7 +57,8 @@ def clean_data(df):
         categories[column] = categories[column].astype(str).str[-1]
         categories[column] = pd.to_numeric(categories[column])
     
-    # Replaces rows with value 2 of 'related' column to 1
+    # Some rows on related column has value of 2
+    # Replace 2 with 1
     categories['related'] = categories['related'].replace(2, 1)
     
     # Replace categories column in df with new category columns
@@ -72,7 +75,14 @@ def clean_data(df):
 # Save the clean dataset into an sqlite database
 def save_data(df, database_filename):
     """
-    Save the clean dataframe into an sqlite database
+    Method for saving the clean dataframe into an sqlite database
+
+    Args:
+        df (pandas dataframe): Sanitized dataframe obtained from 'clean_data'
+        database_filename (str): DB filename
+
+    Output:
+        None
     """
 
     engine = create_engine('sqlite:///{}'.format(database_filename))
