b8a8f213e7fa1840d2ca63ec000b76a62e02eeee
rezaabdullah <air.reza@hotmail.com>
2020-08-08 20:33:36 +0600

    feat: Created routes.py

---
+++ portfolio_analytics_dashboard/dashboard/routes.py
@@ -0,0 +1,12 @@
+from dashboard import app
+
+from flask import render_template
+
+@qpp.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/dashboard-portfolio")
+def dashboard_portfolio():
+    return render_template(dashboard_portfolio.html)
