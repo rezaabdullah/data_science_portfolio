709ddac7e96587e1d181067da9feecea60058c83
rezaabdullah <air.reza@hotmail.com>
2020-08-10 13:30:53 +0600

    feat: Add features to read data

--- portfolio_analytics_dashboard/dashboard/routes.py
+++ portfolio_analytics_dashboard/dashboard/routes.py
@@ -1,5 +1,13 @@
 from dashboard import app
 from flask import render_template
+from data_preprocessing.data_wrangling import clean_data, top_district
+import pandas as pd
+
+# Clean dataset
+clean_df = clean_data()
+
+# Filter out top 5 districts that has the highest number of audience
+topdistrict = top_district(clean_df, 5)
 
 @app.route("/")
 @app.route("/index")
